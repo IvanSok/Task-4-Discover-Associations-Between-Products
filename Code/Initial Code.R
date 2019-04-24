e3be2e2c1d3251d7c017f3e5db4a517c29f5dfdc
Joan Figueras <joanfigueras997@gmail.com>
2019-04-24 15:16:23 +0200

    Base code

--- Code/Initial Code.R
+++ Code/Initial Code.R
@@ -2,7 +2,7 @@ library("pacman")
 p_load(dplyr, ggplot2,corrplot, GGally, readr,caret,readxl,knitr,
        printr,party,polycor,BBmisc,car,reshape,arules,arulesViz)
 
-transactions <- read.transactions("~/Ubiqum/Task 6/ElectronidexTransactions2017.csv", sep = ",",format = "basket",header = FALSE)
+transactions <- read.transactions("Datasets/ElectronidexTransactions2017.csv", sep = ",",format = "basket",header = FALSE)
 
 
 
@@ -12,7 +12,9 @@ inspect(transactions[1:10], itemSep = " + ", setStart = "", setEnd ="", linebrea
 
 itemFrequencyPlot(transactions, horiz = TRUE, type = "absolute",topN = 20,popCol = TRUE)
 
-apriori(transactions)
-Rules<- apriori (transactions, parameter = list(supp = 0.6, conf = 0.8,minlen=2,maxlen=30))
-inspect(Rules)
-
+Rules<- apriori (transactions, parameter = list(supp = 0.03, conf = 0.2,minlen = 2,target = "rules"))
+inspect(sort(Rules,by = "lift"))
+summary(Rules)
+imacrules <- subset(Rules, items %in% "iMac")
+inspect(imacrules)
+plot(Rules)
