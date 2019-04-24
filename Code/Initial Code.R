a4764fb2cf093cad30d79eb9bb266712c395a40d
Ivan Sokolenko <ivan95sokol@gmail.com>
2019-04-24 15:32:28 +0200

    sp

--- Code/Initial Code.R
+++ Code/Initial Code.R
@@ -2,19 +2,23 @@ library("pacman")
 p_load(dplyr, ggplot2,corrplot, GGally, readr,caret,readxl,knitr,
        printr,party,polycor,BBmisc,car,reshape,arules,arulesViz,rstudioapi)
 
-#Setting directory path 
+# Github Setup: 
 current_path <- getActiveDocumentContext()$path
 setwd(dirname(dirname(current_path)))
 rm(current_path)
 
-#Importing the dataset
+# Importing Dataset:
 transactions <- read.transactions("Datasets/ElectronidexTransactions2017.csv", sep = ",",format = "basket",header = FALSE)
 
+# DATA INSPECTION:
 itemLabels(transactions)
 length (transactions)
 inspect(transactions[1:10], itemSep = " + ", setStart = "", setEnd ="", linebreak = FALSE)
 
-itemFrequencyPlot(transactions, horiz = TRUE, type = "absolute",topN = 20,popCol = TRUE)
+# PLOTS:
+itemFrequencyPlot(transactions, horiz = TRUE, type = "absolute",topN = 20)
+image(sample(transactions, 100))
+
 
 Rules<- apriori (transactions, parameter = list(supp = 0.03, conf = 0.2,minlen = 2,target = "rules"))
 inspect(sort(Rules,by = "lift"))
