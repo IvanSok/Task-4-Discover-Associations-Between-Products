6a7cca3d04cf69068b15c14df8ae0c00ecee14de
Joan Figueras <joanfigueras997@gmail.com>
2019-04-25 10:55:13 +0200

    dawda

--- Code/Initial Code.R
+++ Code/Initial Code.R
@@ -8,15 +8,15 @@ setwd(dirname(dirname(current_path)))
 rm(current_path)
 
 # IMPORTING DATASET:
-transactions <- read.transactions("Datasets/ElectronidexTransactions2017.csv", sep = ",",format = "basket",header = FALSE)
-
+transactions <- read.transactions("Datasets/ElectronidexTransactions2017.csv", sep = ",",
+                                  format = "basket",header = FALSE)
 # DATA INSPECTION:
 itemLabels(transactions)
 length (transactions)
 inspect(transactions[1:10], itemSep = " + ", setStart = "",
         setEnd ="", linebreak = FALSE)
 size (transactions) # Number of items per transaction
-LIST(transactions) # Lists the transactions by conversion 
+LIST(transactions[10:100]) # Lists the transactions by conversion 
 
 # PLOTS:
 itemFrequencyPlot(transactions, horiz = TRUE, 
@@ -24,19 +24,21 @@ itemFrequencyPlot(transactions, horiz = TRUE,
 image(sample(transactions, 100))
 
 #Creating rules for the transactions
-Rules <- apriori (transactions, parameter = list(supp = 0.001, 
-                                                 conf = 0.9,minlen = 2,target = "rules"))
-Rules <- Rules[-which(is.redundant(Rules) == TRUE)]
-inspect(sort(Rules,by = "lift"))
-summary(Rules)
-plot(Rules,jitter = 0)
+rules <- apriori (transactions, parameter = list(supp = 0.002, 
+                                                 conf = 0.8,minlen = 2,target = "rules"))
+rules <- rules[-which(is.redundant(rules) == TRUE)]
+inspect(sort(rules,by = "lift"))
+summary(rules)
+plot(rules,jitter = 0)
 
 #Loop to get rules for every subset
 itemrules <- list()
 rules_loop <- c()
 for (i in itemLabels(transactions)) {
-  rules_loop <- subset(Rules, items %in% i)
+  rules_loop <- subset(rules, items %in% i)
   itemrules[[i]] <- rules_loop
 }
-itemrules
+inspect(itemrules$iMac)
 saveRDS(object = itemrules,file = "Models/ItemRulesSubset")
+
+inspectDT(rules)
